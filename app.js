f68ae462ef5638bc6fdde6746106cda0d5ca004d
alex <lexa0303@ukr.net>
2017-06-13 18:04:15 +0300

    dunno

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let fs = require("fs");
 let session = require("express-session");
 let MongoStore = require("connect-mongo")(session);
 const compression = require('compression');
+var minify = require('express-minify');
 
 let functions = require("./functions");
 let config = require("./config");
@@ -29,13 +30,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(compression());
+app.use(minify({
+    cache: './cache',
+    uglifyJS: undefined,
+    cssmin: undefined,
+    onerror: undefined,
+    js_match: /(.*).js/,
+    css_match: /(.*).css/,
+    sass_match: /scss/,
+    less_match: /less/,
+    stylus_match: /stylus/,
+    coffee_match: /coffeescript/,
+    json_match: /json/
+}));
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+    maxage: '0'
+}));
+
 
 let sessionStore = require("./lib/sessionStore");
 
